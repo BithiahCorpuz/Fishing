9e65b148b9dc6b00906c9d24139562cd3f852f5f
bithiah01 <bithiahcorpuz@gmail.com>
2021-11-08 17:06:29 +0800

    Add project files.

---
+++ Fish/Fish/ViewModels/OnboardingViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Fish.Models;
+using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fish.ViewModels
+{
+    public class OnboardingViewModel : BaseViewModel
+    {
+
+        private ObservableRangeCollection<OnboardingModel> items; //collection of Items
+        public ObservableRangeCollection<OnboardingModel> Items { get => items;
+            set => SetProperty(ref items, value);
+        }
+
+        public OnboardingViewModel()
+        {
+                         //create onboarding items
+            Items = new ObservableRangeCollection<OnboardingModel>
+            {
+                new OnboardingModel
+                {
+                    Title = "Fisher Spot",
+                    Content ="Bigmouth buffalo, North American darter mahi-mahi bigeye squaretail surfperch silver dollar." +
+                    " Daggertooth pike conger paddlefish gouramie pearl danio mummichog waryfish grass carp, " +
+                    "common carp striped bass Black swallower graveldiver sabertooth.",
+                    Image =""
+                },
+
+                new OnboardingModel
+                {
+                    Title = "Nature and Fish",
+                    Content ="Bull shark escolar pomfret arapaima daggertooth pike conger sand stargazer," +
+                    " California smoothtongue. Pickerel beluga sturgeon horsefish, Peter's" +
+                    " elephantnose fish threadtail barfish Rattail fierasfer desert pupfish.",
+                    Image =""
+                },
+
+
+                new OnboardingModel
+                {
+                    Title = "Fishing near me",
+                    Content ="Bigmouth buffalo, North American darter mahi-mahi bigeye squaretail surfperch silver dollar. " +
+                    "Daggertooth pike conger paddlefish gouramie pearl danio mummichog waryfish grass carp," +
+                    " common carp striped bass Black swallower graveldiver sabertooth.",
+                    Image =""
+                },
+
+
+               
+            };
+        }
+    }
+}
---
+++ Fish/Fish/Views/OnboardingPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Fish
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OnboardingPage : ContentPage
+    {
+        public OnboardingPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            Radials.RadiusX = Width * 6;
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await FadeBox.FadeTo(1, 1000);
+            await Navigation.PopModalAsync(false); 
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            //Radials.RadiusX = this.Width * e.NewValue;
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
